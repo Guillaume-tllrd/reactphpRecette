c5e002deda5155caa56a22c441dabe08986bab12
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-10-02 12:28:43 +0200

    edit debut

--- src/components/comments/CommentsList.jsx
+++ src/components/comments/CommentsList.jsx
@@ -1,4 +1,4 @@
-// import { useEffect } from 'react';
+import { useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import DeleteComment from './DeleteComment';
 import EditComment from './EditComment';
@@ -12,12 +12,17 @@ const CommentsList = ({comments}) => {
     // useEffect(() => {
     //     dispatch(fetchComment(recipe.id))
     // },[dispatch, recipe])
-
+    const [editToggle, setEditToggle] = useState(false);
+    const [editContent, setEditContent] = useState("");
+    function handleEditToggle(){
+        setEditToggle(!editToggle);
+    }
 
     return (
         <div>
         {comments && comments.length > 0 ? (
           comments.map((comment, index) => (
+            // faire une partie comment
             <div key={index} className='border-b mx-4 my-2'>
               <div className='flex justify-between gap-2 pt-2'> 
                 {/* la div qui permet d'opposer l'espace du nom et de la bulle avec le delete en utilisant le justify-between */}
@@ -30,12 +35,18 @@ const CommentsList = ({comments}) => {
                   <h1 className='font-semibold'>{comment.username}</h1>
                 </div>
                 <div className='flex'>
-                    <EditComment comment={comment}/>
+                    <EditComment comment={comment} handleEditToggle={handleEditToggle}/>
                     <DeleteComment comment={comment}/> 
                 </div>
                 
               </div>
-              <p>{comment.comment}</p>
+              {/* // pour éditer on remet dans un form avec textarea et on lui attavche une fonction */}
+                {editToggle ?  (<form >
+          {/* ne pas oublier de mettre un onChange pour réupérer la valeur */}
+                <textarea autoFocus={true} defaultValue={comment.comment}  onChange={e => setEditContent(e.target.value)}></textarea>
+                <button type='submit'>Edit</button>
+                </form>) : (
+              <p>{comment.comment}</p>)}
               <p className='font-light text-sm py-2'>
                 {new Date(comment.date).toLocaleDateString()}
               </p>
--- src/components/comments/EditComment.jsx
+++ src/components/comments/EditComment.jsx
@@ -1,14 +1,16 @@
 import { Pencil } from 'lucide-react';
 import { useSelector } from 'react-redux';
 
-const EditComment = ({comment}) => {
+const EditComment = ({comment, handleEditToggle}) => {
     const user = useSelector((state) => state.userReducer);
     
     return (
         <div>
+            <button onClick={handleEditToggle}>
             {/* si l'utilisateur connecté est égal à l'id du comment alors tu m'affiches "pencil" pour edit */}
             {user && user.id == comment.user_id &&
             <Pencil/>} 
+            </button>
         </div>
     );
 };
